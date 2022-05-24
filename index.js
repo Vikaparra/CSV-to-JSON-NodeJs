b6f2883a1e89a724132f3539611bb68f941f8aa7
vikaparra <vikaparra@gmail.com>
2022-05-23 21:10:01 -0300

    Parsing Phone

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const PNF = require('google-libphonenumber').PhoneNumberFormat;
-const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+const ppn = require('awesome-phonenumber').parsePhoneNumber;
 const csv = require('csv-parser');
-csv({ separator: '\n' });
 let lodash = require('lodash'); 
 const fs = require('fs');
 const { parse } = require('path');
 const { forEach } = require('lodash');
 const results = [];
-const organizedResults = {};
 
 
 // -------------- pegando dados ----------------
@@ -17,19 +14,23 @@ fs.createReadStream('C:\\Users\\k\\Documents\\Estudos\\Programacao\\NodeJs\\CSV-
   .on('data', (data) => results.push(data))
   .on('end', () => {
 
-    let headers = [];
-    headers = lodash.keys(results[0]);
-    newPerson(results[0]);
+    let pessoas = [];
+    pessoas = lodash.map(results, newPerson);
+
+    let data = JSON.stringify(pessoas);
+    console.log(data);
+
+      const escrita = fs.createWriteStream('output.json');
+      escrita.write(data);
+      escrita.end();
     
   });
 
-  function newPerson(person){
+  function newPerson (person){
       fullname = person.fullname;
       eid = person.eid;
       invisible = person.invisible;
       see_all = person.see_all;
-
-    let pessoas = [];
 
       let pessoa = new Pessoa(fullname,eid,invisible,see_all);
 
@@ -37,28 +38,8 @@ fs.createReadStream('C:\\Users\\k\\Documents\\Estudos\\Programacao\\NodeJs\\CSV-
         pessoa.setAddresses(key, value);
       })
 
-      let pessoa2 = new Pessoa(fullname,eid,invisible,see_all);
-
-      lodash.forEach(person, function(value, key){
-        pessoa2.setAddresses(key, value);
-      })
-
-      pessoas.push(pessoa,pessoa2);
-
-      let data = JSON.stringify(pessoas);
-      console.log(data);
-
-        const escrita = fs.createWriteStream('output.json');
-        escrita.write(data);
-        escrita.end();
-
+      return pessoa;
   }
-
-
-// function parsePhone(phoneNumber){
-//     const number = phoneUtil.parseAndKeepRawInput(phoneNumber, 'BR');
-//     return phoneUtil.formatOutOfCountryCallingNumber(number, 'CH');
-// }
 
 
 // ---------------- estruturas ------------------
@@ -77,10 +58,19 @@ class Pessoa {
     setAddresses(header,address){
         let tags = [];
         tags = lodash.split(header,' ');
-        if((tags[0]==="phone")||(tags[0]==="email")){
-            let adr = new Address(tags[0], address)
+        if(tags[0]==="phone"){
+          const pn = ppn.parsePhoneNumber( address, 'BR' );
+          if(pn.isValid()){
+            let newAddress = pn.getNumber( 'e164' );
+            let adr = new Address(tags[0], newAddress);
             adr.setTags(lodash.drop(tags));
             this.addresses.push(adr);
+          }
+        }
+        else if (tags[0]==="email"){
+          let adr = new Address(tags[0], address)
+          adr.setTags(lodash.drop(tags));
+          this.addresses.push(adr);
         }
     }
 
